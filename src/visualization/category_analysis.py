2f8d5f0753951792081e8444c624a84d74f85022
Judy <32590167@qq.com>
2026-03-23 00:37:52 +0800

    The trend of years

--- src/visualization/category_analysis.py
+++ src/visualization/category_analysis.py
@@ -7,7 +7,7 @@ df = pd.read_csv(r"D:\Studywork\studySpace\douBan\data\proceed\douban_top250_pro
 #各类电影top
 #apply是用于遍历某一列中的所有元素
 df['category']=df['tail'].apply(lambda x:x.split('/')[-1])
-df['category'] = df['category'].apply(lambda x: [i.strip() for i in x.split(' ')])
+df['category'] = df['category'].apply(lambda x: [i.strip() for i in x.split(' ') if i.split()!=''])
 df_explode = df.explode('category')
 category_counts=df_explode['category'].value_counts()
 print(category_counts)
@@ -15,11 +15,30 @@ print(category_counts)
 category_counts.plot(kind='bar',figsize=(12,6))
 plt.xticks(ticks=np.arange(len(category_counts)), labels=category_counts.index, rotation=45)
 plt.title('Movie Categories Frequency')
-plt.xlabel('Category',fontsize=8)
-plt.ylabel('Count',fontsize=8)
+plt.xlabel('Category',fontsize=12)
+plt.ylabel('Count',fontsize=12)
 plt.tight_layout()
 plt.show()
 #-------------各类电影Top------------#
 df_sorted= df_explode.sort_values(['category','rating'], ascending=[True,False])
 category_top=df_sorted.groupby('category').head(5)
 print(category_top)
+#-------------每类电影的平均评分------------#
+category_mean=df_explode.groupby('category')['rating'].mean().sort_values(ascending=False)
+print(category_mean)
+#-----画图------
+category_mean.plot(kind='bar',figsize=(12,6))
+plt.xticks(ticks=np.arange(len(category_mean)), labels=category_mean.index, rotation=45)
+plt.title('Movie Categories Name')
+plt.xlabel('Category',fontsize=12)
+plt.ylabel('Mean Rating',fontsize=12)
+plt.tight_layout()
+plt.show()
+#---------------------年份趋势分析------------------
+#--提取年份
+df['year']=df["year"].astype(str).str.extract(r'(\d{4})')
+#--缺失值检查--0
+print(df["year"].isna().sum())
+#groupby锁定整个表，不用单独一列
+year_mean=df.groupby("year")["rating"].mean()
+print(year_mean)
